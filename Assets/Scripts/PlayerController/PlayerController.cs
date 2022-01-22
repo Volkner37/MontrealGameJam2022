8cf4c8af1ee7e04287eb2c62c19adb144128be2d
Charles-Antoine <charles.antoine.bellerive.dev@gmail.com>
2022-01-21 22:26:41 -0500

    Player Prefab.

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -11,8 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float forwardAcceleration = 1;
     [SerializeField] private float backwardAcceleration = 1;
     [SerializeField] private float sideAcceleration = 1;
-    [SerializeField] private float forwardDeceleration = 1;
-    [SerializeField] private float sideDeceleration = 1;
+    [SerializeField] private float airControlRatio = 0.3f;
 
     [Space]
     [Header("Global Values")] 
@@ -26,14 +25,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float forceMagnet;
     [SerializeField] private float forceMagnetObject;
     [SerializeField] private AnimationCurve staticAcceleration;
-    [SerializeField] private bool DebugMode = true;
+    [SerializeField] private bool debugMode = true;
 
     
     private Camera _camera;
     private Rigidbody _rigidbody;
+    private PhysicMaterial _physicsMaterial;
     private Vector3 _inputDirection;
-    private Vector3 _dampeningDirection;
-    private bool _grounded = false;
+    private bool _isGrounded = false;
 
     #region GunLogics  
     private bool _IsTryingToRepel = false;
@@ -44,39 +43,59 @@ public class PlayerController : MonoBehaviour
     private bool _IsRepeling => _IsTryingToRepel && !_RepelLocked;
     private bool _IsUsingGun => _IsRepeling || _IsAttracting;
     #endregion
+    
+    #region Inputs
+
+    private float _VecticalAxis;
+    private float _HorizontalAxis;
+    private bool _isJumping;
+
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         _camera = GetComponentInChildren<Camera>();
         _rigidbody = GetComponent<Rigidbody>();
+        _physicsMaterial = GetComponent<CapsuleCollider>().material;
     }
 
+    void Update()
+    {
+        UpdateInputs();
+    }
+
+    private void UpdateInputs()
+    {
+        _VecticalAxis = Input.GetAxis("Vertical");
+        _HorizontalAxis = Input.GetAxis("Horizontal");
+        _IsTryingToAttract = Input.GetMouseButton(0);
+        _IsTryingToRepel = Input.GetMouseButton(1);
+        
+        if(!_isJumping)
+            _isJumping = Input.GetKeyDown(KeyCode.Space);
+    }
+    
     void FixedUpdate()
     {
         UpdateGrounded();
-        UpdateMouseInput();
 
         if(!_IsUsingGun)
         {
+            _physicsMaterial.dynamicFriction = 2;
+            
             UpdateInputDirection();
-            UpdateDampening();    
             CheckJumpInput();
-
-            _rigidbody.AddForce((_inputDirection + ( _grounded ? _dampeningDirection : Vector3.zero )) * Time.deltaTime, ForceMode.VelocityChange);
+            
+            _rigidbody.AddForce(_inputDirection * Time.deltaTime, ForceMode.Impulse);
         }
         else
         {
-            if (_IsAttracting) // left click 
-            {        
-                UpdateMagnetGunEffect();
-                _RepelLocked = true;
-            }
-            if (_IsRepeling) // right click
-            {
-                UpdateMagnetGunEffect();
-                _AttractLocked = true;
-            }
+            _physicsMaterial.dynamicFriction = 0;
+            _RepelLocked = _IsAttracting;
+            _AttractLocked = _IsRepeling;
+            UpdateMagnetGunEffect();
         }
 
         if (Input.GetMouseButtonUp(0)) // Pulling button UP
@@ -90,7 +109,7 @@ public class PlayerController : MonoBehaviour
 
         _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, maxSpeed);
         
-        if(DebugMode)
+        if(debugMode)
             ShowDebug();
     }
 
@@ -98,51 +117,51 @@ public class PlayerController : MonoBehaviour
     {
         RaycastHit hitGround;
         int layerMask = ~LayerMask.GetMask("Player");
-        _grounded = Physics.Raycast(_rigidbody.transform.position, new Vector3(0,-1,0), out hitGround, 1.2f, layerMask) ? true : false;
+        _isGrounded = Physics.Raycast(transform.position, new Vector3(0, -1, 0), out hitGround, 1.5f, layerMask);
     }
 
     private void UpdateInputDirection()
     {
         //Accelerations
-        float vertical = Input.GetAxis("Vertical");
-        if (vertical >= 0)
-            vertical *= forwardAcceleration;
-        else if (vertical <= 0)
-            vertical *= backwardAcceleration;
-
-        float horizontal = Input.GetAxis("Horizontal");
-        horizontal *= sideAcceleration;
+        _VecticalAxis = Input.GetAxis("Vertical");
+        if (_VecticalAxis >= 0)
+            _VecticalAxis *= forwardAcceleration;
+        else if (_VecticalAxis <= 0)
+            _VecticalAxis *= backwardAcceleration;
+        
+        _HorizontalAxis *= sideAcceleration;
 
         //For diagonal speeds
         float maxDiagonalSpeed = Mathf.Max(forwardAcceleration, backwardAcceleration, sideAcceleration);
-        _inputDirection = Vector3.ClampMagnitude((new Vector3(_camera.transform.right.x,0,_camera.transform.right.z)) * horizontal + (new Vector3(_camera.transform.forward.x,0,_camera.transform.forward.z)) * vertical, maxDiagonalSpeed);
-    }
-
-    private void UpdateDampening()
-    {
-        _dampeningDirection = _rigidbody.velocity * (-1 * forwardDeceleration);
+        
+        if (!_isGrounded)
+        {
+            _VecticalAxis *= airControlRatio;
+            _HorizontalAxis *= airControlRatio;
+            maxDiagonalSpeed *= airControlRatio;
+        }
+        
+        _inputDirection = Vector3.ClampMagnitude((new Vector3(_camera.transform.right.x,0, _camera.transform.right.z)) * _HorizontalAxis + (new Vector3(_camera.transform.forward.x,0, _camera.transform.forward.z)) * _VecticalAxis, maxDiagonalSpeed);
     }
 
     private void CheckJumpInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _grounded)
+        if (_isJumping && _isGrounded)
         {
             _rigidbody.AddForce(Vector3.up * (forceJump * Time.deltaTime), ForceMode.Impulse);
+            _isJumping = false;
         }
     }
 
-    private void UpdateMouseInput()
-    {
-        _IsTryingToAttract = Input.GetMouseButton(0);
-        _IsTryingToRepel = Input.GetMouseButton(1);
-    }
+    
 
     private void ShowDebug()
     {
         #region DebugRays
-        Debug.DrawRay(transform.position,  _inputDirection * 2.0f, Color.green);
-        Debug.DrawRay(transform.position,  _rigidbody.velocity * 2.0f, Color.blue);
-        Debug.DrawRay(transform.position, _dampeningDirection, Color.red);
+
+        var position = transform.position;
+        Debug.DrawRay(position,  _inputDirection * 2.0f, Color.green);
+        Debug.DrawRay(position,  _rigidbody.velocity * 2.0f, Color.blue);
         Debug.DrawRay(_camera.transform.position, _camera.transform.forward * 3f, Color.yellow);
         #endregion
         
@@ -150,7 +169,7 @@ public class PlayerController : MonoBehaviour
         if(_IsAttracting)
             Debug.Log("Attracting");
         if(_IsRepeling)
-            Debug.Log("Repeling");
+            Debug.Log("Repelling");
         #endregion
 
     }
@@ -165,19 +184,17 @@ public class PlayerController : MonoBehaviour
         { 
             GameObject objectHit = hit.transform.gameObject;
 
-            if(objectHit.GetComponent<Magnetic>())
+            Magnetic magneticComponent = objectHit.GetComponent<Magnetic>();
+            if(magneticComponent is {IsStatic: true})
             {
-                Magnetic interable = objectHit.GetComponent<Magnetic>();
-                if(interable.IsStatic)
-                {
-                    float acceleration = staticAcceleration.Evaluate((hit.distance)/maxRange);
-                    _rigidbody.AddForce((_IsAttracting? _camera.transform.forward : -_camera.transform.forward) * (acceleration * forceMagnet * Time.deltaTime) , ForceMode.Impulse);
-                }
+                float acceleration = staticAcceleration.Evaluate((hit.distance)/maxRange);
+                _rigidbody.AddForce((_IsAttracting? _camera.transform.forward : -_camera.transform.forward) * (acceleration * forceMagnet * Time.deltaTime) , ForceMode.Force);
             }
         }
-        else{
-                _RepelLocked = false;
-                _AttractLocked = false;
+        else
+        {
+            _RepelLocked = false;
+            _AttractLocked = false;
         }
 
     }
