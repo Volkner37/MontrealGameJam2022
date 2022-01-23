612045027df0d83b8117d5c4272c6966cb3e5d34
yobiscus <jo@stashed.dev>
2022-01-22 22:41:40 -0500

    MovingPlatform: Add Lock Waypoint feature to preserve relative waypoint positions
    When the waypoints are locked, moving the platform will also move the
    waypoints.
    
    Errors will be emitted in the logs if a platform's waypoints are not locked. This
    is to prevent last minute bugs where the platform is moved but the waypoints are
    not reset correctly.
    
    Closes #52

--- Assets/Scripts/MovingPlatformEditor.cs
+++ Assets/Scripts/MovingPlatformEditor.cs
@@ -12,11 +12,36 @@ public class MovingPlatformEditor : Editor
         MovingPlatform script = (MovingPlatform)target;
         DrawDefaultInspector();
 
+        if (script.WaypointsLocked)
+        {
+            EditorGUILayout.HelpBox(
+                "The waypoints are locked. Unlock them to move the platform without changing " +
+                "affecting the waypoints.",
+                MessageType.Info);
+            if (GUILayout.Button("Unlock Waypoints"))
+            {
+                script.WaypointsLocked = false;
+            }
+            GUI.enabled = false;
+        }
+        else
+        {
+            EditorGUILayout.HelpBox(
+                "The waypoints are not locked. Moving the platform will not move the " +
+                "platform with it. Lock the waypoints using the button below once they " +
+                "are placed.",
+                MessageType.Warning);
+            if (GUILayout.Button("Lock Waypoints"))
+            {
+                script.WaypointsLocked = true;
+            }
+        }
+
         EditorGUILayout.HelpBox(
             "The platform will always start in the Moving state from waypoint 0 to 1. " +
             "Use 'Set Waypoint #' buttons below to set waypoint to current location of " +
             "the GameObject. Use 'Snap to Waypoint #' buttons to snap the GameObject to " +
-            "the selected waypoint",
+            "the selected waypoint.",
             MessageType.Info);
 
         for (int b = 0; b < script.GetWaypointsLength(); ++b) {
@@ -26,11 +51,9 @@ public class MovingPlatformEditor : Editor
             }
         }
 
-        for (int b = 0; b < script.GetWaypointsLength(); ++b) {
-            if (GUILayout.Button("Snap to Waypoint " + b.ToString()))
-            {
-                script.SnapToWaypoint(b);
-            }
+        if (script.WaypointsLocked)
+        {
+            GUI.enabled = true;
         }
     }
 }
