0d30b066d696ff9919beabf93bafc5e0bccb9077
Charles-Antoine <charles.antoine.bellerive.dev@gmail.com>
2022-01-29 20:00:43 -0500

    Music Manager

--- Assets/Scripts/Utils/SceneLoaderUtils.cs
+++ Assets/Scripts/Utils/SceneLoaderUtils.cs
@@ -9,17 +9,33 @@ namespace Assets.Scripts.Utils
     {
         const int MaxLevelNumber = 0;
 
+        
         public static void LoadScene(string sceneName)
         {
             //SceneManager.LoadScene(sceneName);
             Debug.Log(sceneName);
             LevelChanger _levelChanger = LevelChanger.Instance;
+            OnSceneLoaded.Invoke(null, new SceneLoadEventArgs(sceneName));
             _levelChanger.FadeToLevel(sceneName);
         }
 
         public static void LoadNextScene()
         {
-            string[] values = SceneManager.GetActiveScene().name.Split('_');
+            string nextScene = FindNextSceneName();
+            LoadScene(nextScene);
+        }
+
+        public static int GetSceneIndex(string name)
+        {
+            int result =SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/FINAL/Levels/{name}.unity");
+            return result < 0 ? 1 : result;
+        }
+
+        private static string FindNextSceneName()
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+            
+            string[] values = currentScene.Split('_');
             int currentNumber = int.Parse(values[1]);
             
             int newNumber = currentNumber+1;
@@ -29,11 +45,21 @@ namespace Assets.Scripts.Utils
             if (newNumber < 10)
                 result = "0" + newNumber;
 
-            string nextScene = values[0] + "_" + result;
+            string computedName = values[0] + "_" + result;
+
+            return GetSceneIndex(computedName) < 0 ? "MainMenu" : computedName;
+        }
+
+        public static event EventHandler<SceneLoadEventArgs> OnSceneLoaded = delegate {  };
+
+        public class SceneLoadEventArgs : EventArgs
+        {
+            public SceneLoadEventArgs(string name)
+            { 
+                Name = name;
+            }
             
-            int sceneIndex = SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/FINAL/Levels/{nextScene}.unity");
-            
-            LoadScene(sceneIndex >= 0 ? nextScene : "MainMenu");
+            public readonly string Name;
         }
     }
 }
