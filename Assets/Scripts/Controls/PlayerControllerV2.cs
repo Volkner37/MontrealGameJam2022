8e816eed71d81dee137108973b6dde36871aa057
Charles-Antoine <charles.antoine.bellerive.dev@gmail.com>
2022-01-30 16:30:27 -0500

    Revert "Fix?"
    This reverts commit 5c1f580c9e812a7cc61c2703cb90a51f93cd2f2d.

--- Assets/Scripts/Controls/PlayerControllerV2.cs
+++ Assets/Scripts/Controls/PlayerControllerV2.cs
@@ -490,17 +490,12 @@ public class PlayerControllerV2 : MonoBehaviour
             {
                 ChangeParent(result ? hit.transform.gameObject : null);
             }
-
+            
             IsGroundedOnMetal = hit.transform.TryGetComponent<Magnetic>(out _);
         }
         else
         {
             IsGroundedOnMetal = false;
-            
-            if (!IsSticked)
-            {
-                ChangeParent(null);
-            }
         }
 
         IsGrounded = grounded;
